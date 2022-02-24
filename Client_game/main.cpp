3cd27dd60f84680cd50929571f1c4768e8bc1301
kalchenko.vladislaw <kalchenko.vladislaw@yandex.ru>
2022-02-24 14:34:40 +0300

    test

--- Client_game/main.cpp
+++ Client_game/main.cpp
@@ -10,17 +10,21 @@ int main()
     sf::UdpSocket socket;
 
     // bind the socket to a port
-    if (socket.bind(54000) != sf::Socket::Done)
+    if (socket.bind(9993) != sf::Socket::Done)
     {
         // error...
         shape.setFillColor(sf::Color::Red);
     }
 
-    char data[100] = "Hellow World!";
+    char data[100];
 
     // UDP socket:
-    sf::IpAddress recipient = "127.0.0.1";
-    unsigned short port = 54000;
+    sf::IpAddress recipient = "172.22.15.218";
+    unsigned short port = 9993;
+   
+    sf::Clock clock;
+
+    sprintf_s<100>(data, "%f", clock.getElapsedTime().asSeconds());
     if (socket.send(data, 100, recipient, port) != sf::Socket::Done)
     {
         shape.setFillColor(sf::Color::Red);
@@ -30,14 +34,24 @@ int main()
         shape.setFillColor(sf::Color::Green);
 
     }
-
     while (window.isOpen())
     {
+        
         sf::Event event;
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
                 window.close();
+        }
+        sprintf_s<100>(data, "%f", clock.getElapsedTime().asSeconds());
+        if (socket.send(data, 100, recipient, port) != sf::Socket::Done)
+        {
+            shape.setFillColor(sf::Color::Red);
+        }
+        else {
+
+            shape.setFillColor(sf::Color::Green);
+
         }
 
         window.clear();
